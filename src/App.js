97d77f8d75a55a224a5748cc48e07bf9d95e1ad1
Thomas Von Dollen <vondot@sunypoly.edu>
2023-03-02 22:15:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes["app-wrapper"]}>
+      <div className={classes["profile-wrapper"]}>
+        <img
+          className={classes["profile-photo"]}
+          alt="profile_photo"
+          src={require("./assets/penguin_placeholder.png")}
+        />
+        <h1>Penguin :)</h1>
+        <h2>Links</h2>
+        <ul>
+          <li>Instagram</li>
+          <li>Twitter</li>
+          <li>LinkedIn</li>
+        </ul>
+      </div>
     </div>
   );
 }
