993d82051c55260474435c04a95486cb3c8c6d22
tmv518 <tmv810@gmail.com>
2023-06-04 20:04:16 -0400

    Worked on UserAuthPage.js design

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { auth } from "./firebase/firebase-config.js";
+import UserAuthPage from "./UserAuthPage";
 
 function App() {
   //custom hook from react-color-palette
@@ -61,145 +62,9 @@ function App() {
   );
   const linkList = useSelector((state) => state.linkList.linkList);
 
-  //AUTHENTICATION
-  const [signUpEmail, setSignUpEmail] = useState("");
-  const [signUpPassword, setSignUpPassword] = useState("");
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-
-  //current user
-  const [user, setUser] = useState("");
-
-  //sets error text
-  const [errorText, setErrorText] = useState("");
-
-  //onAuthStateChanged() is like useEffect()
-  //It is run when the auth state changes
-  useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      setUser(auth.currentUser);
-    });
-  }, []);
-
-  //creates user account
-  const signUp = async (event) => {
-    event.preventDefault();
-    try {
-      setErrorText("");
-      const user = await createUserWithEmailAndPassword(
-        auth,
-        signUpEmail,
-        signUpPassword
-      );
-    } catch (error) {
-      console.log(error.message);
-      console.log("error.code: " + error.code);
-      switch (error.code) {
-        case "auth/email-already-in-use": {
-          console.log("400 ERROR");
-          setErrorText("Email already in use");
-          break;
-        }
-        case "auth/invalid-email": {
-          setErrorText("Enter a valid email");
-          break;
-        }
-      }
-    }
-  };
-
-  //logs user in user account
-  const login = async (event) => {
-    event.preventDefault();
-    setErrorText("");
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
-    } catch (error) {
-      console.log(error.message);
-      console.log("error.code: " + error.code);
-      switch (error.code) {
-        case "auth/user-not-found": {
-          setErrorText("Email/password combination not found");
-          break;
-        }
-        case "auth/invalid-email": {
-          setErrorText("Enter a valid email");
-          break;
-        }
-        case "auth/wrong-password": {
-          setErrorText("Email/Password combination does not match an account");
-          break;
-        }
-        case "auth/missing-password": {
-          setErrorText("Enter a valid password");
-          break;
-        }
-      }
-    }
-  };
-
-  const logout = async () => {
-    await signOut(auth);
-  };
-
   return (
     <div className={classes["app-wrapper"]}>
-      <p>Sign Up</p>
-      <form
-        onSubmit={(e) => {
-          signUp(e);
-        }}
-      >
-        <input
-          type="text"
-          placeholder="username"
-          id="username__signup"
-          onChange={(e) => {
-            setSignUpEmail(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          id="password__signup"
-          onChange={(e) => {
-            setSignUpPassword(e.target.value);
-          }}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>Log In</p>
-      <form
-        onSubmit={(e) => {
-          login(e);
-        }}
-      >
-        <input
-          type="text"
-          placeholder="username"
-          id="username__login"
-          onChange={(e) => {
-            setLoginEmail(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          id="password__login"
-          onChange={(e) => {
-            setLoginPassword(e.target.value);
-          }}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>{errorText}</p>
-      <p>Currently logged-in user: {user?.email}</p>
-      <button onClick={logout}>Log Out</button>
-
+      <UserAuthPage />
       <Router>
         <Link to="edit">
           <button>Edit</button>
---
+++ src/UserAuthPage.js
@@ -0,0 +1,198 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "./firebase/firebase-config.js";
+import { useState, useEffect } from "react";
+import classes from "./UserAuthPage.module.css";
+
+const UserAuthPage = () => {
+  //AUTHENTICATION
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const [signUpPage, setSignUpPage] = useState(true);
+  const [loginPage, setLoginPage] = useState(false);
+
+  const signUpPageHandler = () => {
+    setSignUpPage(true);
+    setLoginPage(false);
+  };
+
+  const loginPageHandler = () => {
+    setLoginPage(true);
+    setSignUpPage(false);
+  };
+
+  //current user
+  const [user, setUser] = useState("");
+
+  //sets error text
+  const [errorText, setErrorText] = useState("");
+
+  //onAuthStateChanged() is like useEffect()
+  //It is run when the auth state changes
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(auth.currentUser);
+    });
+  }, []);
+
+  //creates user account
+  const signUp = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      setErrorText("");
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        signUpEmail,
+        signUpPassword
+      );
+    } catch (error) {
+      console.log(error.message);
+      console.log("error.code: " + error.code);
+      switch (error.code) {
+        case "auth/email-already-in-use": {
+          console.log("400 ERROR");
+          setErrorText("Email already in use");
+          break;
+        }
+        case "auth/invalid-email": {
+          setErrorText("Enter a valid email");
+          break;
+        }
+      }
+    }
+
+    setLoading(false);
+  };
+
+  //logs user in user account
+  const login = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setErrorText("");
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+    } catch (error) {
+      console.log(error.message);
+      console.log("error.code: " + error.code);
+      switch (error.code) {
+        case "auth/user-not-found": {
+          setErrorText("Email/password combination not found");
+          break;
+        }
+        case "auth/invalid-email": {
+          setErrorText("Enter a valid email");
+          break;
+        }
+        case "auth/wrong-password": {
+          setErrorText("Email/Password combination does not match an account");
+          break;
+        }
+        case "auth/missing-password": {
+          setErrorText("Enter a valid password");
+          break;
+        }
+      }
+    }
+    setLoading(false);
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <div className={classes["auth__wrapper"]}>
+      <div className={classes["top-buttons"]}>
+        <button
+          style={{
+            borderBottom: signUpPage ? "1px solid blue" : "none",
+          }}
+          onClick={signUpPageHandler}
+        >
+          Sign Up
+        </button>
+        <button onClick={loginPageHandler}>Login</button>
+      </div>
+      {signUpPage && (
+        <>
+          <p>Sign Up</p>
+          <form
+            className={classes["signup__wrapper"]}
+            onSubmit={(e) => {
+              signUp(e);
+            }}
+          >
+            <input
+              type="text"
+              placeholder="E-Mail"
+              id="e-mail__signup"
+              onChange={(e) => {
+                setSignUpEmail(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password__signup"
+              onChange={(e) => {
+                setSignUpPassword(e.target.value);
+              }}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      )}
+
+      {loginPage && (
+        <>
+          <p>Log In</p>
+          <form
+            onSubmit={(e) => {
+              login(e);
+            }}
+            className={classes["login__wrapper"]}
+          >
+            <input
+              type="text"
+              placeholder="E-Mail"
+              id="e-mail__login"
+              onChange={(e) => {
+                setLoginEmail(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password__login"
+              onChange={(e) => {
+                setLoginPassword(e.target.value);
+              }}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      )}
+      {loading && <div className={classes["auth__overlay"]}></div>}
+
+      <p>{errorText}</p>
+      <p>Currently logged-in user: {user?.email}</p>
+      <button onClick={logout}>Log Out</button>
+    </div>
+  );
+};
+
+export default UserAuthPage;
